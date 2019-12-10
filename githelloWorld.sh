64f4bfec500eb88bd206342bef9c6c56bddb046f
Ubuntu <DevOpsTeam5@DevOpsTeam5.kvdh40tkdpvu5caa5sytas315g.tx.internal.cloudapp.net>
2019-12-10 09:16:53 +0000

    modified1

--- githelloWorld.sh
+++ githelloWorld.sh
@@ -1,1 +1,7 @@
-echo "hello github... this is my first program"
+xMerge branch 'master' of https://github.com/Laharivegiraju22/devops
+
+# Please enter a commit message to explain why this merge is necessary,
+# especially if it merges an updated upstream into a topic branch.
+#
+# Lines starting with '#' will be ignored, and an empty message aborts
+# the commit.
